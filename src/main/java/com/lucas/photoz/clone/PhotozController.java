744085b4aca410f826e69e80c4b258b49ad79e56
rodrigues-i <lucas-f250@live.com>
2022-07-06 16:51:06 -0300

    Create a get method to get an image by id and a delete method to delete an image by id

--- src/main/java/com/lucas/photoz/clone/PhotozController.java
+++ src/main/java/com/lucas/photoz/clone/PhotozController.java
@@ -1,14 +1,24 @@
 package com.lucas.photoz.clone;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PhotozController {
 
-	private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+	private Map<String, Photo> db = new HashMap<>() {
+		{
+			put("1", new Photo("1", "hello.jpg"));
+		}
+	};
 
 	@GetMapping("/")
 	public String hello() {
@@ -16,7 +26,22 @@ public class PhotozController {
 	}
 
 	@GetMapping("/photoz")
-	public List<Photo> get() {
-		return db;
+	public Collection<Photo> get() {
+		return db.values();
+	}
+
+	@GetMapping("/photoz/{id}")
+	public Photo get(@PathVariable String id) {
+		Photo photo = db.get(id);
+		if (photo == null)
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		return photo;
+	}
+
+	@DeleteMapping("/photoz/{id}")
+	public void delete(@PathVariable String id) {
+		Photo photo = db.remove(id);
+		if (photo == null)
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 	}
 }
